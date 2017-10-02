01b041fc312a1e6b02f857cd38ea4ac93a85e9a2
garyemerson <gmohammadioun@gmail.com>
2017-10-01 20:43:34 -0700

    implement dynamic programming soln that needs optimization

--- src/main.rs
+++ src/main.rs
@@ -5,28 +5,86 @@ use std::cmp::min;
 
 fn main() {
     let num_test_cases = get_line().trim().parse::<i32>().unwrap();
-    for _ in 0..num_test_cases {
-        let info = read_test_case();
-        let soln = solve_brute_force(info.0, info.1, &info.2);
-        println!("{}", soln.len());
-        for placement in soln {
-            // print_placement(info.0, info.1, placement, &info.2);
-            // println!("---");
+    // println!("{} test cases", num_test_cases);
+    let mut soln_counts = Vec::new();
+    for test_case_num in 0..num_test_cases {
+        let info: (i32, i32, HashSet<(i32, i32)>) = read_test_case();
+        // let soln = solve_brute_force(info.0, info.1, &info.2);
+        println!("\x1b[1mTEST CASE {}\x1b[0m\n", test_case_num + 1);
+        let soln = solve(info.0, info.1, &info.2);
+        for (i, placements) in soln.iter().enumerate() {
+            println!("{} solns with {} queen{}", placements.len(), i + 1, if i > 0 { "s" } else { "" });
+            // for plc in placements {
+            //     print_placement(info.0, info.1, plc, &info.2);
+            //     println!("---");
+            // }
+            // println!();
         }
-        // println!();
-        // solve(info.0, info.1, info.2);
+        println!(
+            "{} total solutions\n\n",
+            soln.iter().fold(0, |acc, v| acc + v.len()));
+        soln_counts.push(
+            soln.iter().fold(0, |acc, v| acc + v.len()));
+    }
+
+    for c in &soln_counts {
+        println!("{}", c);
     }
 }
 
-// fn solve(rows: i32, cols: i32, blocked_positions: HashSet<(i32, i32)>) -> Vec<(i32, i32)> {
-//     Vec::new()
-// }
+fn solve(rows: i32, cols: i32, blocked_positions: &HashSet<(i32, i32)>) -> Vec<Vec<HashSet<(i32, i32)>>> {
+    let mut all_placements: Vec<Vec<HashSet<(i32, i32)>>> = Vec::new();
+    let mut new_placements: Vec<HashSet<(i32, i32)>> = get_placements_from_basis(rows, cols, &vec![HashSet::new()], blocked_positions);
+    loop {
+        if new_placements.len() > 0 {
+            all_placements.push(new_placements.clone());
+        } else {
+            break;
+        }
+        new_placements = get_placements_from_basis(rows, cols, &new_placements, blocked_positions);
+    }
+    all_placements
+}
 
-fn print_placement(rows: i32, cols: i32, occupied_positions: Vec<(i32, i32)>, blocked_positions: &HashSet<(i32, i32)>) {
-    let positions_set: HashSet<(i32, i32)> = HashSet::from_iter(occupied_positions.into_iter());
+fn get_placements_from_basis(
+    rows: i32,
+    cols: i32,
+    basis_placements: &Vec<HashSet<(i32, i32)>>,
+    blocked_positions: &HashSet<(i32, i32)>) -> Vec<HashSet<(i32, i32)>>
+{
+    let mut placements = Vec::new();
+    println!("iterating over {} basis placements", basis_placements.len());
+    for plc in basis_placements {
+        let taken_rows: HashSet<i32> = HashSet::from_iter(plc.iter().map(|pair| pair.0));
+        let taken_cols: HashSet<i32> = HashSet::from_iter(plc.iter().map(|pair| pair.1));
+        let blocked_rows: HashSet<i32> = HashSet::from_iter(blocked_positions.iter().map(|pair| pair.0));
+        let blocked_cols: HashSet<i32> = HashSet::from_iter(blocked_positions.iter().map(|pair| pair.1));
+        for row in (0..rows).filter(|r| !taken_rows.contains(r) || blocked_rows.contains(r)) {
+            for col in (0..cols).filter(|c| !taken_cols.contains(c) || blocked_cols.contains(c)) {
+                if !plc.contains(&(row, col)) {
+                    let mut new_placement: HashSet<(i32, i32)> = plc.clone();
+                    new_placement.insert((row, col));
+                    if is_valid_placement(
+                        rows,
+                        cols,
+                        &new_placement.clone().into_iter().collect::<Vec<(i32, i32)>>(),
+                        blocked_positions)
+                    {
+                        if !placements.contains(&new_placement) {
+                            placements.push(new_placement);
+                        }
+                    }
+                }
+            }
+        }
+    }
+    placements
+}
+
+fn print_placement(rows: i32, cols: i32, occupied_positions: &HashSet<(i32, i32)>, blocked_positions: &HashSet<(i32, i32)>) {
     for row in 0..rows {
         for col in 0..cols {
-            if positions_set.contains(&(row, col)) {
+            if occupied_positions.contains(&(row, col)) {
                 print!("Q ");
             } else if blocked_positions.contains(&(row, col)) {
                 print!("# ");
@@ -44,12 +102,12 @@ fn solve_brute_force(rows: i32, cols: i32, blocked_positions: &HashSet<(i32, i32
     placements
         .into_iter()
         .filter(|positions|
-            is_valid_placement(rows, cols, positions.to_vec(), blocked_positions))
+            is_valid_placement(rows, cols, &positions.to_vec(), blocked_positions))
         .collect::<Vec<Vec<(i32, i32)>>>()
 }
 
-fn is_valid_placement(rows: i32, cols: i32, positions: Vec<(i32, i32)>, blocked_positions: &HashSet<(i32, i32)>) -> bool {
-    let positions_set: HashSet<(i32, i32)> = HashSet::from_iter(positions.into_iter());
+fn is_valid_placement(rows: i32, cols: i32, positions: &Vec<(i32, i32)>, blocked_positions: &HashSet<(i32, i32)>) -> bool {
+    let positions_set: HashSet<(i32, i32)> = HashSet::from_iter(positions.clone());
     for pos in &positions_set {
         if !(is_valid_path(get_right_horizontal(*pos, rows, cols), &positions_set, blocked_positions) &&
             is_valid_path(get_down_horizontal(*pos, rows, cols), &positions_set, blocked_positions) &&
