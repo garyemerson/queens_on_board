23f06b5f4a0fd3181a3420ef359fc37791eb8312
garyemerson <gmohammadioun@gmail.com>
2017-10-08 14:10:38 -0700

    add recursive DFS implementation

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,9 @@ fn main() {
     for test_case_num in 0..num_test_cases {
         let info: (i32, i32, HashSet<(i32, i32)>) = read_test_case();
         // let soln = solve_brute_force(info.0, info.1, &info.2);
-        println!("\x1b[1mTEST CASE {}\x1b[0m\n", test_case_num + 1);
-        let soln = solve(info.0, info.1, &info.2);
+        // println!("\x1b[1mTEST CASE {}\x1b[0m\n", test_case_num + 1);
+        // let soln = solve(info.0, info.1, &info.2);
+        let soln = solve2(info.0, info.1, &info.2);
         // for (i, placements) in soln.iter().enumerate() {
         //     println!("{} solns with {} queen{}", placements.len(), i + 1, if i > 0 { "s" } else { "" });
         //     // print_placement(info.0, info.1, &HashSet::from_iter(placements.clone()), &info.2);
@@ -30,9 +31,9 @@ fn main() {
         //     soln.len());
 
         // optimal
-        println!(
-            "{} total solutions\n\n",
-            soln/*.iter().fold(0, |acc, v| acc + v.len())*/);
+        // println!(
+        //     "{} total solutions\n\n",
+        //     soln/*.iter().fold(0, |acc, v| acc + v.len())*/);
         soln_counts.push(
             soln/*.iter().fold(0, |acc, v| acc + v.len())*/);
     }
@@ -53,6 +54,46 @@ fn solve(rows: i32, cols: i32, blocked_positions: &HashSet<(i32, i32)>) -> usize
     count
 }
 
+fn solve2(rows: i32, cols: i32, blocked_positions: &HashSet<(i32, i32)>) -> i32 {
+    num_placements_from_basis(
+        rows,
+        cols,
+        -1,
+        &mut HashSet::new(),
+        blocked_positions)
+}
+
+fn num_placements_from_basis(
+    rows: i32,
+    cols: i32,
+    max_position_index: i32,
+    basis_placement: &mut HashSet<(i32, i32)>,
+    blocked_positions: &HashSet<(i32, i32)>) -> i32
+{
+    let mut num_placements = 0;
+    for position_index in (max_position_index + 1)..(rows * cols) {
+        let row = position_index / cols;
+        let col = position_index % cols;
+        if can_add_pos((row, col), rows, cols, basis_placement, blocked_positions) {
+            num_placements += 1;
+            basis_placement.insert((row, col));
+            num_placements += num_placements_from_basis(rows, cols, position_index, basis_placement, blocked_positions);
+            basis_placement.remove(&(row, col));
+        }
+    }
+    num_placements
+}
+
+fn print_in_order(rows: i32, cols: i32, placement: &HashSet<(i32, i32)>) {
+    let mut clone = placement.clone();
+    let mut pairs: Vec<(i32, i32)> = clone.drain().collect();
+    pairs.sort_by_key(|p| p.0 * cols + cols);
+    for p in pairs {
+        print!("({}, {}) ", p.0, p.1);
+    }
+    println!();
+}
+
 fn get_placements_from_basis(
     rows: i32,
     cols: i32,
@@ -66,7 +107,7 @@ fn get_placements_from_basis(
             for col in 0..cols {
                 if !plc.0.contains(&(row, col)) && row * cols + col > plc.1  {
                     let mut new_placement: (HashSet<(i32, i32)>, i32) = plc.clone();
-                    if can_add_pos(rows, cols, &new_placement.0, blocked_positions, (row, col)) {
+                    if can_add_pos((row, col), rows, cols, &new_placement.0, blocked_positions) {
                         new_placement.0.insert((row, col));
                         new_placement.1 = row * cols + col;
                         placements.push(new_placement);
@@ -78,7 +119,7 @@ fn get_placements_from_basis(
     placements
 }
 
-fn can_add_pos(rows: i32, cols: i32, placement: &HashSet<(i32, i32)>, blocked_positions: &HashSet<(i32, i32)>, pos: (i32, i32)) -> bool {
+fn can_add_pos(pos: (i32, i32), rows: i32, cols: i32, placement: &HashSet<(i32, i32)>, blocked_positions: &HashSet<(i32, i32)>) -> bool {
     let positions_set: HashSet<(i32, i32)> = placement.clone();
     is_right_horizontal_valid(pos, rows, cols, &positions_set, blocked_positions) &&
         is_down_horizontal_valid(pos, rows, cols, &positions_set, blocked_positions) &&
